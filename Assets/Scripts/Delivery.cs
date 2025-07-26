acd0e885c751c30adef482ef969766264df7eb06
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-07-26 17:13:56 -0400

    Organizing hierarchy

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,22 +3,24 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
-
+    [SerializeField] float destroyDelay = 1.0f;
     void OnCollisionEnter2D(Collision2D collision)
     {
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Package")
+        if (collision.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package has been picked up!");
             hasPackage = true;
+            Destroy(collision.gameObject, destroyDelay);
         }
 
         if (collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package has been delivered!");
+            hasPackage = false;
         }
     }
 }
