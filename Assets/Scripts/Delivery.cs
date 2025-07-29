7e1b7fc536e12955880bf527210f1edb3319bb05
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-07-28 22:53:17 -0400

    Change car's color wether hasPackage is True

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,15 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage;
     [SerializeField] float destroyDelay = 1.0f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
 
@@ -14,6 +23,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package has been picked up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(collision.gameObject, destroyDelay);
         }
 
@@ -21,6 +31,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package has been delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
