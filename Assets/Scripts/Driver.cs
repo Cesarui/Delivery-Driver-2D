f43213b3f7c5aac1686b6711d4e2993eca04d3dd
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-09-01 17:02:26 -0400

    Final touch, boosts and bumps

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,6 +4,9 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float turnSpeed = 0.1f;
     [SerializeField] float moveSpeed = 0.1f;
+    [SerializeField] float slowSpeed;
+    [SerializeField] float boostSpeed;
+
     void Start()
     {
         
@@ -16,5 +19,16 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -turnAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bump")
+        {
+            moveSpeed = slowSpeed;
+        }
+        else if (collision.gameObject.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
